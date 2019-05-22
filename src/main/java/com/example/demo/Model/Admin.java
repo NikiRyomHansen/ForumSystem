1d81538bb571f38d792dbcd7c3f65e95e2da9537
NikiRyomHansen <nikihansen@hotmail.dk>
2019-05-22 11:42:53 +0200

    Added PostMapping request for additional user info. Refactored permission so it is now 0 if a User is constructed and 1 if an Admin is constructed

--- src/main/java/com/example/demo/Model/Admin.java
+++ src/main/java/com/example/demo/Model/Admin.java
@@ -6,9 +6,11 @@ import javax.persistence.Entity;
 public class Admin extends Person {
 
     //Constructor containing all fields from Person super class
+    //Setting permission to 1 to indicate that this Person is an Admin
     public Admin(int personID, String username, String passwordPerson, String email,
-                 byte[] picture, String description, int permission) {
-        super(personID, username, passwordPerson, email, picture, description, permission);
+                 byte[] picture, String description) {
+        super(personID, username, passwordPerson, email, picture, description);
+        setPermission(1);
     }
 
 
